c0f41cbfd35aa838bb0cb7df6abb636174d117ca
satoshiyr16 <satoshi.trip.1@gmail.com>
2023-09-23 17:34:08 +0900

    topPageのtitle、subtitle追加

---
+++ src/resources/views/topPage.blade.php
@@ -0,0 +1,20 @@
+<html>
+  <head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+JP&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/topPage.css') }}">
+    @vite(['resources/css/app.css', 'resources/scss/app.scss', 'resources/js/app.js'])
+  </head>
+  <button class="hamburger">
+    <span class="hamburger_bar"></span>
+    <span class="hamburger_bar"></span>
+    <span class="hamburger_bar"></span>
+  </button>
+  <div class="topPage_area">
+    <div class="title_area">
+      <h1 class="title">雑魚の産声</h1>
+      <p class="sub_title">未経験エンジニアの成り上がり記録</p>
+    </div>
+  </div>
+</html>

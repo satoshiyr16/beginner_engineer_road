36a4f79d0a64eb6c38374af66ca86c07d20c625a
satoshiyr16 <satoshi.trip.1@gmail.com>
2023-11-06 23:07:30 +0900

    :recycle: css設計を意識したclass名変更

--- src/resources/views/topPage.blade.php
+++ src/resources/views/topPage.blade.php
@@ -5,12 +5,15 @@
   @endsection
   @section('content')
 {{--      <div id="app"></div>--}}
-      <div class="topPage_area">
-        <div class="title_area">
-          <div class="title_section">
-            <h1 class="title">雑魚の産声</h1>
-            <p class="sub_title">未経験エンジニアの成り上がり記録</p>
+      <div class="l-topPage_area">
+        <div class="p-title_area">
+          <div class="p-title_section">
+            <h1 class="p-title">雑魚の産声</h1>
+            <p class="p-sub_title">未経験エンジニアの成り上がり記録</p>
           </div>
+        </div>
+        <div class="c-postContent_area">
+
         </div>
       </div>
 

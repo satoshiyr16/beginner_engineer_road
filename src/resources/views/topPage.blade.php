b7322793d98e691532e26cda25f2e0ed68e07753
satoshiyr16 <satoshi.trip.1@gmail.com>
2023-12-14 01:47:26 +0900

    :smile: トップページにroading画面と波線背景追加

--- src/resources/views/topPage.blade.php
+++ src/resources/views/topPage.blade.php
@@ -2,14 +2,22 @@
   @extends('layouts.app')
   @section('additionHeader')
     <link rel="stylesheet" href="{{ asset('css/topPage.css') }}">
+    <script src="{{ asset('js/topPage.js') }}"></script>
   @endsection
+  <div id="l-loading">
+    <div id="c-loading_text">0%</div>
+    <div id="c-loading_bar"></div>
+    <div id="c-loading_bar_overlap"></div>
+</div>
   @section('content')
-{{--      <div id="app"></div>--}}
       <div class="l-topPage_area">
         <div class="p-title_area">
           <div class="p-title_section">
             <h1 class="p-title">雑魚の産声</h1>
             <p class="p-sub_title">未経験エンジニアの成り上がり記録</p>
+            <div class="wave">
+              <canvas id="waveCanvas"></canvas>
+            </div>
           </div>
         </div>
         <div class="c-postContent_area">

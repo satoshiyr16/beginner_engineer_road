985a2b7a63e480a56d17fb1d8b9ceb166955f885
satoshiyr16 <satoshi.trip.1@gmail.com>
2024-01-14 22:53:31 +0900

    :smile: header部分の追加

---
+++ src/resources/js/Vue/components/headers/headerMenu.vue
@@ -0,0 +1,161 @@
+<template>
+  <div class="l-nav_area">
+    <div class="c-button_area">
+      <button class="p-nav_button" @click="openNav" :class="{ 'close': menuClose }">
+        <span class="p-nav_bar"></span>
+        <span class="p-nav_bar"></span>
+        <span class="p-nav_bar"></span>
+      </button>
+    </div>
+    <nav class="p-nav_content" :class="{ 'panelactive': navActive }">
+      <div class="c-button_area">
+        <span class="c-button_close" @click="closeNav"></span>
+      </div>
+      <ul class="p-nav_list">
+        <a href="/">
+          <li>TOP</li>
+        </a>
+        <a href="/add_post">
+          <li>POST</li>
+        </a>
+        <li>う</li>
+        <li>え</li>
+        <li>お</li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      navActive: false,
+      menuClose: false,
+    };
+  },
+  methods: {
+    openNav() {
+      this.navActive = true;
+      this.menuClose = true;
+    },
+    closeNav() {
+      this.navActive = false;
+      this.menuClose = false;
+    }
+  }
+}
+</script>
+
+<style>
+button {
+  border-color: transparent;
+  background: transparent;
+  cursor: pointer;
+}
+
+li {
+  list-style: none;
+}
+
+.close {
+  opacity: 0;
+  pointer-events: none;
+}
+
+.c-button_area {
+  display: flex;
+  justify-content: end;
+
+  .p-nav_button {
+    width: 40px;
+    height: 32px;
+    position: relative;
+    margin-top: 30px;
+    margin-right: 30px;
+  }
+
+  .p-nav_bar {
+    display: block;
+    width: 100%;
+    height: 4px;
+    position: absolute;
+    left: 0;
+    background: #000;
+
+    &:nth-child(1) {
+      top: 0;
+    }
+
+    &:nth-child(2) {
+      top: 50%;
+      transform: translateY(-50%);
+    }
+
+    &:nth-child(3) {
+      top: 100%;
+      transform: translateY(-100%);
+    }
+  }
+}
+
+.p-nav_content {
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  z-index: -999;
+  opacity: 0;
+  background-color: rgba(173, 216, 230, 0.9);
+  transition: opacity 0.5s ease;
+
+  .c-button_area {
+    height: 10%;
+    margin: 30px 35px;
+  }
+
+  .p-nav_list {
+    height: 60%;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+}
+
+.panelactive {
+  z-index: 999;
+  opacity: 1;
+}
+
+.c-button_close {
+  display: block;
+  position: relative;
+  width: 30px;
+  height: 30px;
+}
+
+.c-button_close::before,
+.c-button_close::after {
+  content: "";
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 8px;
+  height: 40px;
+  background: #333;
+  cursor: pointer;
+}
+
+.c-button_close::before {
+  transform: translate(-50%, -50%) rotate(45deg);
+}
+
+.c-button_close::after {
+  transform: translate(-50%, -50%) rotate(-45deg);
+}
+</style>
--- src/resources/js/Vue/components/layouts/header.vue
+++ src/resources/js/Vue/components/layouts/header.vue
@@ -1,5 +1,58 @@
 <template>
-  <router-link to="/tasks" >タスク一覧</router-link>
-  <router-link to="/tasks/create">タスク追加</router-link>
+  <div class="l-header_container">
+    <wave-component></wave-component>
+    <div class="l-logo_area">
+      <img class="p-logo" src="/images/zakoubu_logo.jpg">
+    </div>
+     <div class="l-title_area">
+        <p class="p-title">雑魚の産声</p>
+        <p class="p-sub_title">駆け出しエンジニアの成り上がり記録</p>
+     </div>
+     <div class="l-menu_area">
+      <header-menu-component></header-menu-component>
+     </div>
+    </div>
 </template>
 
+<script>
+  import waveComponent from '../headers/wave.vue';
+  import headerMenuComponent from '../headers/headerMenu.vue';
+
+
+  export default {
+  components: {
+    waveComponent,
+    headerMenuComponent
+  }
+}
+</script>
+
+<style>
+.l-header_container {
+  width: 100%;
+  max-height: 20vh;
+  display: flex;
+  justify-content: space-between;
+
+.l-logo_area {
+  .p-logo {
+    margin: 20px 0 0 20px;
+    width: 50px;
+    height: 50px;
+  }
+}
+  .l-title_area {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+
+    .p-title {
+      font-size: 2rem;
+    }
+    .p-sub_title {
+      font-size: 1rem;
+    }
+  }
+}
+</style>
